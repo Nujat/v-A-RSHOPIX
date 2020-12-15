dbec705729cd42a7a5fddc841dcc8576220badaf
ep34 <33396274+ep34@users.noreply.github.com>
2020-12-15 16:11:06 +0600

    Add files via upload

---
+++ Scripts/CubeDekha.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CubeDekha : MonoBehaviour
+{
+
+    public GameObject canvas;
+
+
+
+    public void cubeDekhabe()
+    {
+        canvas.SetActive(false);
+    }
+
+
+}
